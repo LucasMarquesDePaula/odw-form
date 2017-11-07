427160f3798c0e9995c975ae6071c0a9d3bd50de
LucasMarquesDePaula <marques.analista@gmail.com>
2017-11-07 09:16:10 -0200

    Textarea created

--- src/main.prod.js
+++ src/main.prod.js
@@ -6,6 +6,7 @@ import Number from "@/components/fields/input/number/Number"
 import Overlay from "@/components/overlay/Overlay"
 import Select from "@/components/fields/select/Select"
 import Switch from "@/components/fields/Switch"
+import Textarea from "@/components/fields/Textarea"
 import TimePicker from "@/components/fields/input/TimePicker"
 
 function install(Vue) {
@@ -18,6 +19,7 @@ function install(Vue) {
     Overlay,
     Select,
     Switch,
+    Textarea,
     TimePicker
   ]
 
